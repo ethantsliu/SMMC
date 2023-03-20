b92a12f7c6ed50ccaae02b07fc24580d6cc664f4
Ethan <ethantsliu@gmail.com>
2023-03-20 02:05:40 +0000

    Updated val

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 pip install --upgrade pip
 pip install transformers timm fairscale 
 pip install --upgrade openai salesforce-lavis
-pip install whisper word2number 
+pip install word2number 
 pip install decord webdataset opencv-python einops omegaconf pandas iopath spacy pycocoevalcap
 
 conda install -c conda-forge pycocotools
 
 apt-get install unzip
 apt install libgl1-mesa-glx
+
